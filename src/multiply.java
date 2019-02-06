8ea2b8a222acd462744e4863eb9d357ad134e6cf
mlkrajewski <miko.krajewski@gmail.com>
2019-02-06 15:13:39 -0800

    check

--- src/multiply.java
+++ src/multiply.java
@@ -3,7 +3,7 @@ public class multiply {
 	public static void main(String[] args) {
 		//prints the product of 6 and 5
 		int i = 6, j = 5;
-		System.out.println("The product of " + i + " and " + j + " is " + (i * j));
+		System.out.println("The product of " + i + " and " + j + " is " + (i * j) + ".");
 	}
 
 }
