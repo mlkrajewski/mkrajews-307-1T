bf7d90747b561a805971f2a52115e29c826464d4
mlkrajewski <miko.krajewski@gmail.com>
2019-02-06 15:02:03 -0800

    start

---
+++ src/multiply.java
@@ -0,0 +1,9 @@
+
+public class multiply {
+	public static void main(String[] args) {
+		//prints the product of 6 and 5
+		int i = 6, j = 5;
+		System.out.println("The product of " + i + " and " + j + " is " + (i * j));
+	}
+
+}
